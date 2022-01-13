d60739ee7fb94cd6199321465540f5ac2977801b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-01-12 21:40:07 -0500

    :wrench: Add Eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["svelte3", "@typescript-eslint"],
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
+    },
+  ],
+  settings: {
+    "svelte3/typescript": true,
+  },
+};
